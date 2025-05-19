ed3f05947422ae8f25f0faaa69b6ceefa56ef362
wsb347 <wsb347@naver.com>
2025-05-19 21:52:28 +0900

    팀과제 5번 파일 업로드

---
+++ teamsub5/FrontEnd/main.py
@@ -0,0 +1,31 @@
+# ex4.py
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("input.html")
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    name = request.form["name"]
+    student_number = request.form["student_number"]
+    gender = request.form["gender"]
+    major = request.form["major"]
+    languages = request.form.getlist("languages")
+
+    return render_template(
+        "result.html",
+        name=name,
+        student_number=student_number,
+        gender=gender,
+        major=major,
+        languages=", ".join(languages),
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
